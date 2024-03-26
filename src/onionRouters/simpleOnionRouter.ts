d6ea2c37e17b5fd7f79ff2e4b84db4d0a6778c4c
Anne-Solène <anne-solene.izard@edu.devinci.fr>
2024-03-26 23:45:34 +0100

    first commit

--- src/onionRouters/simpleOnionRouter.ts
+++ src/onionRouters/simpleOnionRouter.ts
@@ -7,8 +7,28 @@ export async function simpleOnionRouter(nodeId: number) {
   onionRouter.use(express.json());
   onionRouter.use(bodyParser.json());
 
+  let lastReceivedEncryptedMessage : string | null = null;
+  let LastReceivedDecryptedMessage: string | null = null;
+  let LastMessageDestination : string | null = null;
+
   // TODO implement the status route
   // onionRouter.get("/status", (req, res) => {});
+
+  onionRouter.get("/status", (req, res) => {
+    res.send("live");
+  });
+
+  onionRouter.get("/getLastReceivedEncryptedMessage", (req, res) => {
+    res.json({ result: lastReceivedEncryptedMessage });
+  });
+
+  onionRouter.get("/getLastReceivedDecryptedMessage", (req, res) => {
+    res.json({ result: LastReceivedDecryptedMessage });
+  }); 
+
+  onionRouter.get("/getLastMessageDestination", (req, res) => {
+    res.json({ result: LastMessageDestination });
+  });
 
   const server = onionRouter.listen(BASE_ONION_ROUTER_PORT + nodeId, () => {
     console.log(
